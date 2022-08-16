dc8be5e4e4dfbdc1931f2205a83d4ebdf3a14607
Cyril Isip <cisip@Cyrils-MacBook-Pro.local>
2022-08-15 21:18:07 -0500

    fixed cors issue and added redis

--- server/index.js
+++ server/index.js
@@ -1,20 +1,42 @@
-const express = require('express')
-const fetch = require('node-fetch')
-const app = express()
+const express = require('express');
+const redis = require('redis');
+const fetch = require('node-fetch');
+const app = express();
 const port = 3000;
+const redis_port = process.env.redis_port || 6379
+require('dotenv').config()
 
-app.get('/api/:date', async (req, res, body) => {
-    fetch(`https://api.nasa.gov/planetary/apod?api_key=VSDIcILkW1pYNnyUHRjtAYIa2285Q2TbLH4QkzFj&date=${req.params.date}`)
-    
-    .then(response => response.json())
-    .then(data => {
+const client = redis.createClient(redis_port);
+
+
+function cache(res, req, next) {
+  const { date } = req.params
+
+  client.get(date, (error, data) => {
+    if (error) throw error;
+
+    if (!data) {
       res.send(data)
-    })
-    .catch(err => console.log(err))
-   
-    
+    } else {
+      next();
+    }
+  })
+}
+app.get('/api/:date', cache, async (req, res) => {
+  try {
+    const { date } = req.params
+
+    const response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${process.env.API_KEY}&date=${date}`);
+
+    const data = await response.json();
+
+    res.send(data)
+  } catch (e) {
+    console.log(e)
+  }
 })
-  
+
+
 app.listen(port, () => {
-console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${port}`)
 })
